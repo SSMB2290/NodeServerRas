cf7ce6fdf37e7f1061a6b09564ea7795bc9af98c
BharathTangella634 <tangellabharathkumar@gmail.com>
2025-03-10 03:01:03 +0530

    Update Chatbot.js

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -120,7 +120,7 @@ console.log(finalPrompt);
 
     try {
      
-      const rasaResponse = await fetch("http://localhost:5005/webhooks/rest/webhook", {
+      const rasaResponse = await fetch("https://rasa-8.onrender.com/webhooks/rest/webhook", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ sender: "user", message: userMessage }),
